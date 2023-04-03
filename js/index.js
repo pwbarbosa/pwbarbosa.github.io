1787011636bef0a27a7a7e6a35fb58a524c35f74
Pedro <pwbarbosa@gmail.com>
2023-04-02 23:43:12 -0400

    code cleanup

--- js/index.js
+++ js/index.js
@@ -1,7 +1,4 @@
 function myFunction(x) {
     x.classList.toggle("change");
     $('.top-nav').toggleClass('off-canvas on-canvas');
-    setTimeout(function(){
-      $('.top-nav a').removeClass('display-none');
-    },250)
   } 
